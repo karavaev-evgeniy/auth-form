e464ad2de3f8b20bb291f62d1fb96d356f0357b8
Evgeniy Karavaev <karavaev.evgeniy@gmail.com>
2024-09-06 19:22:17 +0700

    Доработал адаптивность

--- src/app/pages/LoginPage/LoginPage.tsx
+++ src/app/pages/LoginPage/LoginPage.tsx
@@ -5,7 +5,7 @@ function LoginPage() {
 	return (
 		<main className="login-page">
 			<div className="login-page__grid">
-				<h1 className="login-page__title">Please log in...</h1>
+				<h1 className="login-page__title">Please log&nbsp;in...</h1>
 				<AuthForm />
 			</div>
 		</main>
